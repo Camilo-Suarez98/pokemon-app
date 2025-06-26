330de24c58c504642842e533d0b9aa4e0f4b3752
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-26 15:50:02 -0500

    include styles in modal

--- src/components/PokemonModal.tsx
+++ src/components/PokemonModal.tsx
@@ -1,51 +1,92 @@
 import React from 'react';
+import { Heart, Shield, Sword, Zap } from 'lucide-react';
 
 import type { PokemonModalProps } from '../types/pokemon';
 
 const PokemonModal: React.FC<PokemonModalProps> = ({ pokemon, onClose }) => {
+  const getStatIcon = (statName: string): React.ReactElement | null => {
+    switch (statName) {
+      case "hp":
+        return <Heart className="w-4 h-4 inline mr-1" />;
+      case "attack":
+        return <Sword className="w-4 h-4 inline mr-1" />;
+      case "defense":
+        return <Shield className="w-4 h-4 inline mr-1" />;
+      case "special-attack":
+        return <Sword className="w-4 h-4 inline mr-1" />;
+      case "special-defense":
+        return <Shield className="w-4 h-4 inline mr-1" />;
+      case "speed":
+        return <Zap className="w-4 h-4 inline mr-1" />;
+      default:
+        return null;
+    }
+  };
+
+  const lowAndHighStats = (quantity: number) => {
+    if (quantity < 50) {
+      return "text-red-500"
+    } else if (quantity > 70) {
+      return "text-green-500"
+    } else {
+      return "text-black"
+    }
+  }
+
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="close-button" onClick={onClose}>&times;</button>
-        <div className="pokemon-modal-header">
-          <h2 className="pokemon-name">{pokemon.name}</h2>
-          <span className="pokemon-id">#{pokemon.id.toString().padStart(3, '0')}</span>
+    <div className="fixed inset-0 w-full h-full flex justify-center items-center z-50 bg-black/80" onClick={onClose}>
+      <div className="bg-white relative rounded-lg p-6 max-w-3xl w-11/12 max-h-[90vh] overflow-y-auto" onClick={(e) => e.stopPropagation()}>
+        <button className="absolute top-2 right-2 bg-transparent border-none text-2xl cursor-pointer text-gray-500" onClick={onClose}>&times;</button>
+        <div className="flex items-center mb-4 gap-4">
+          <h2 className="font-semibold text-2xl capitalize flex items-center gap-2">
+            {pokemon.name}
+            <span className="text-lg text-[#64748b]">#{pokemon.id.toString().padStart(3, '0')}</span>
+          </h2>
         </div>
-        <div className="pokemon-modal-image">
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-            alt={pokemon.name}
-          />
-        </div>
-        <div className="pokemon-modal-info">
-          <div className="pokemon-types">
-            <h3>Types:</h3>
-            <div className="type-list">
-              {pokemon.types.map((type) => (
-                <span key={type.slot} className={`type-badge ${type.type.name}`}>
-                  {type.type.name}
-                </span>
-              ))}
+
+        <div className='grid items-center md:grid-cols-2 gap-6'>
+          <div className="text-center">
+            <div className="relative w-48 h-48 mx-auto">
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+                alt={pokemon.name}
+              />
+            </div>
+            <div>
+              <div className="flex flex-wrap gap-2 justify-center mb-4">
+                {pokemon.types.map((type) => (
+                  <span key={type.slot} className={`type-badge ${type.type.name} text-white font-bold`}>
+                    {type.type.name}
+                  </span>
+                ))}
+              </div>
+              <div className="grid grid-cols-2 gap-4 text-sm">
+                <div>
+                  <p className='font-semibold'>Height:</p>
+                  <p>{pokemon.height / 10} m</p>
+                </div>
+                <div>
+                  <p className='font-semibold'>Weight:</p>
+                  <p>{pokemon.weight / 10} kg</p>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="pokemon-stats">
-            <h3>Stats:</h3>
+          <div>
+            <h3 className="font-semibold tracking-tight text-2xl capitalize flex items-center gap-2">Base Stats:</h3>
             {pokemon.stats.map((stat) => (
-              <div key={stat.stat.name} className="stat-item">
-                <span className="stat-name">{stat.stat.name}:</span>
-                <span className="stat-value">{stat.base_stat}</span>
+              <div key={stat.stat.name} className="flex justify-between my-2">
+                <span className="capitalize font-medium">
+                  {getStatIcon(stat.stat.name)}
+                  {stat.stat.name}:
+                </span>
+                <span
+                  className={`font-semibold ${lowAndHighStats(stat.base_stat)}`}
+                >
+                  {stat.base_stat}
+                </span>
               </div>
             ))}
-          </div>
-          <div className="pokemon-details">
-            <div className="detail-item">
-              <span>Height:</span>
-              <span>{pokemon.height / 10} m</span>
-            </div>
-            <div className="detail-item">
-              <span>Weight:</span>
-              <span>{pokemon.weight / 10} kg</span>
-            </div>
           </div>
         </div>
       </div>
