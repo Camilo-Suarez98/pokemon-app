71372c3b1cbe0518b89a8c7b5c6d8281aec30c8c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-26 11:08:07 -0500

    edit grid

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -5,6 +5,7 @@ import PokemonModal from './PokemonModal.tsx';
 
 const PokemonGrid: React.FC<PokemonGridProps> = ({ pokemons }) => {
   const [selectedPokemon, setSelectedPokemon] = useState<PokemonType | null>(null);
+  const [quantityOfPokemon, setQuantityOfPokemon] = useState(40);
 
   const openModal = (pokemon: PokemonType) => {
     setSelectedPokemon(pokemon);
@@ -17,15 +18,15 @@ const PokemonGrid: React.FC<PokemonGridProps> = ({ pokemons }) => {
   return (
     <div className="p-5">
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))] gap-4">
-        {pokemons.map((pokemon) => (
+        {pokemons.slice(0, quantityOfPokemon).map((pokemon) => (
           <div
             key={pokemon.id}
-            className="relative bg-white rounded-lg p-4 cursor-pointer hover:shadow-lg transition-all duration-500 hover:scale-105"
+            className="relative rounded-lg border p-6 bg-card text-card-foreground shadow-sm cursor-pointer hover:shadow-lg transition-shadow duration-200"
             onClick={() => openModal(pokemon)}
           >
             <span className="text-gray-700 absolute top-4 left-4">#{pokemon.id}</span>
             <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
               alt={pokemon.name}
               className="w-24 h-24 m-auto block"
             />
@@ -35,6 +36,15 @@ const PokemonGrid: React.FC<PokemonGridProps> = ({ pokemons }) => {
           </div>
         ))}
       </div>
+      {pokemons.length > 0 && (
+        <button
+          className="px-4 py-2 mt-4 rounded-md bg-blue-500 text-white disabled:opacity-50"
+          onClick={() => setQuantityOfPokemon(quantityOfPokemon + 40)}
+          disabled={quantityOfPokemon >= pokemons.length}
+        >
+          Load More
+        </button>
+      )}
       {selectedPokemon && (
         <PokemonModal
           pokemon={selectedPokemon}
